c4db19e042bb8d881f417ca224560f342c1550b2
YaelAke <al068350@uacam.mx>
2025-03-04 22:50:32 -0600

    Segundo Commit

--- obj/Debug/net8.0/MiApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8f6b4c762a85d706c8e8b01346783a9aaa4443")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
